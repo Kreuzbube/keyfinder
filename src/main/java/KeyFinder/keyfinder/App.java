93ca8ce7d01947b4601246c40efffff5a5951f8a
Xaver Schiener <Xaver Schiener>
2015-11-03 18:39:03 +0100

    First commit

---
+++ src/main/java/KeyFinder/keyfinder/App.java
@@ -0,0 +1,13 @@
+package KeyFinder.keyfinder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
