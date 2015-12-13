3b491d6433c6d18766b57d9323c33720aa59b49c
Oschm <oschmidt@hm.edu>
2015-12-13 13:54:12 +0100

    Updated Main Method

--- src/main/java/KeyFinder/keyfinder/App.java
+++ src/main/java/KeyFinder/keyfinder/App.java
@@ -1,14 +1,34 @@
-package KeyFinder.keyfinder;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!2" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Crawler crawler = new Crawler();
+		int starterId = 0;
+			try {
+				 starterId =crawler.mainPage(new URL("http://pr0gramm.com/api/items/get?flags=7"));
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		if (starterId ==0){
+			System.out.println("Fehler");
+			return;
+		}
+		try {
+			crawler.crawl(starterId);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
